daa5ca33a4e4ce5e8a37b1bc3a97d533bc3a837b
Andre Pacak <pacak@uni-mainz.de>
2020-07-07 11:19:45 +0200

    data for benchmarking 500 commits of project keras (only compare files that have different content)

--- truediff/src/test/scala/truediff/python/BenchmarkCommits.scala
+++ truediff/src/test/scala/truediff/python/BenchmarkCommits.scala
@@ -6,6 +6,8 @@ import truechange.EditScript
 import truediff.util.BenchmarkUtils._
 import truediff.util.CSVUtil.csvRowToString
 
+import scala.collection.mutable
+
 
 object BenchmarkCommits extends App {
   val projectName = "keras"
@@ -19,12 +21,13 @@ object BenchmarkCommits extends App {
     val commits = rootDir.listFiles()
       .filter(_.getName.startsWith(s"$projectName-"))
       .sortBy(getCommitNumber)
-      .take(100)
 
     val prevCommit: Map[File, File] = commits.tail.zipWithIndex.map { case (commit, i) =>
       commit -> commits(i)
     }.toMap
 
+
+    val parsedFiles:mutable.Map[File, String] = mutable.Map()
     // iterate over commits
     // iterate over all files of commit and check if previous has this file
     // parse both
@@ -35,18 +38,27 @@ object BenchmarkCommits extends App {
         val prevCommitFile = new File(prevCommit(commit), file.getName)
         if (prevCommitFile.exists()) {
           val currCommitFileContent = readFile(file.getAbsolutePath)
-          val prevCommitFileContent = readFile(prevCommitFile.getAbsolutePath)
-          val (tree, (editscript, _), _, diffTimes) = timed[(Ast.file, Ast.file), (EditScript, Ast.file)](
-            () => {
-              val currTree = Statements.parse(currCommitFileContent)
-              val prevTree = Statements.parse(prevCommitFileContent)
-              (prevTree, currTree)
-            },
-            (in: (Ast.file, Ast.file)) => in._1.compareTo(in._2)
-          )
-          val res = Some(Measurement(s"${commit.getName}/${file.getName}", tree._1.treesize, tree._1.treeheight, tree._2.treesize, tree._2.treeheight, diffTimes, editscript))
-          println(csvRowToString(res.get.csv))
-          res
+          parsedFiles(file) = currCommitFileContent
+          parsedFiles.get(prevCommitFile) match {
+            case Some(content) => // do nothing
+            case None =>
+              val content = readFile(prevCommitFile.getAbsolutePath)
+              parsedFiles(prevCommitFile) = content
+          }
+
+          if (parsedFiles(file) != parsedFiles(prevCommitFile)) {
+            val (tree, (editscript, _), _, diffTimes) = timed[(Ast.file, Ast.file), (EditScript, Ast.file)](
+              () => {
+                val currTree = Statements.parse(parsedFiles(file))
+                val prevTree = Statements.parse(parsedFiles(prevCommitFile))
+                (prevTree, currTree)
+              },
+              (in: (Ast.file, Ast.file)) => in._1.compareTo(in._2)
+            )
+            val res = Some(Measurement(s"${commit.getName}/${file.getName}", tree._1.treesize, tree._1.treeheight, tree._2.treesize, tree._2.treeheight, diffTimes, editscript))
+            println(csvRowToString(res.get.csv))
+            res
+          } else Option.empty[Measurement]
         } else Option.empty[Measurement]
       }
     }
@@ -65,6 +77,6 @@ object BenchmarkCommits extends App {
   val measurementsPerFile = measurements.map ( m => m.name -> m).toMap
 
   // write data
-  writeFile("benchmark/measurements/python_keras_measurements.csv", measurementsToCSV(measurements))
-  writeFile("benchmark/measurements/python_keras_changing_measurements.csv", measurementsToCSV(changingMeasurements))
+  writeFile("benchmark/measurements/python_keras_500_measurements.csv", measurementsToCSV(measurements))
+  writeFile("benchmark/measurements/python_keras_500_changing_measurements.csv", measurementsToCSV(changingMeasurements))
 }
